348435b4017e9c14f37794cc7424c9b12e779d09
Baden <baden03@gmail.com>
2016-11-22 12:41:01 +0100

    released version 1.7.6

--- print-o-matic.php
+++ print-o-matic.php
@@ -4,7 +4,7 @@ Plugin Name: Print-O-Matic
 Text Domain: print-o-matic
 Plugin URI: http://plugins.twinpictures.de/plugins/print-o-matic/
 Description: Shortcode that adds a printer icon, allowing the user to print the post or a specified HTML element in the post.
-Version: 1.7.6d
+Version: 1.7.6
 Author: twinpictures
 Author URI: http://twinpictuers.de
 License: GPL2
@@ -20,7 +20,7 @@ class WP_Print_O_Matic {
 	 * Current version
 	 * @var string
 	 */
-	var $version = '1.7.6d';
+	var $version = '1.7.6';
 
 	/**
 	 * Used as prefix for options entry
